f671142ddf76c5f8ebd5d46172d2f0c4c19bc119
abuSayedsayed <mdsaied386@gmail.com>
2021-08-09 17:07:29 +0600

    Oh Email feature Updated

---
+++ js/smtp.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', function (event) {
+	event.preventDefault()
+	const name = form.name.value
+	const email = form.email.value
+	const subject = form.subject.value
+	const description = form.description.value
+	const isEmailValid = validate(email)
+	if (isEmailValid) {
+		const mailData = {
+			name,
+			email,
+			subject,
+			description,
+		}
+		sendMail(mailData)
+	} else {
+		alert('Email Is Not Correct')
+	}
+	console.log(isEmailValid)
+})
+
+function sendMail(mailData) {
+	Email.send({
+		Host: 'smtp.gmail.com',
+		Username: 'Sayed123dev',
+		Password: 'hellodev1234',
+		To: 'mdsaied386@gmail.com',
+		From: 'mdsaied386@gmail.com',
+		Subject: `${mailData.name} Send You A Message`,
+		Body: `
+        <h3>${mailData.name} Wanted To Tell You About ....</h3> <br><br>
+        <h5>${mailData.subject} On The Subject</h5> br><br>
+        <p> Message : ${mailData.description} </p>
+         
+        
+        `,
+	}).then((message) => alert(message))
+}
